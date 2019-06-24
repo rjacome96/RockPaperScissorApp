b66ef282773a7048b40790e97e3d230189019a46
Roy O. Jacome <rjacomejr96@aol.com>
2019-06-24 09:42:05 -0400

    added ID to rock image and changed method names.

--- src/application/RockPaperScissorMain.java
+++ src/application/RockPaperScissorMain.java
@@ -4,16 +4,13 @@ import controller.StartScreenController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
-import javafx.scene.layout.AnchorPane;
 
 public class RockPaperScissorMain extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			AnchorPane root = new AnchorPane();
-			Scene startScreen = new Scene(root,400,400);
+			Scene startScreen = StartScreenController.getStartScreenScene();
 			startScreen.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			startScreen = new StartScreenController().startHere(startScreen);
 			primaryStage.setTitle("Welcome!");
 			primaryStage.setScene(startScreen);
 			primaryStage.show();
--- src/controller/StartScreenController.java
+++ src/controller/StartScreenController.java
@@ -2,21 +2,54 @@ package controller;
 
 import java.io.IOException;
 
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 
 public class StartScreenController extends AnchorPane {
-	public Scene startHere(Scene startScreen) {
+	
+	@FXML
+	ImageView rockImage;
+	
+	public static Scene getStartScreenScene() {
+		return new StartScreenController().createStartScreen();
+	}
+	
+	private Scene createStartScreen() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/StartScreenView.fxml"));
 		loader.setController(this);
 		loader.setRoot(this);
+		Scene startScreen = null;
 		try {
 			startScreen = new Scene(loader.load());
 		} catch (IOException e1) {
 			e1.printStackTrace();
 		}
 		
+		Image rock = new Image("File:gameImages/RockImage.jpg");
+		
+		// Testing out Canvas and GraphicsContent
+		Canvas canvas = new Canvas(400, 200);
+		GraphicsContext gc = canvas.getGraphicsContext2D();
+	    gc.setFill(Color.RED);
+	    gc.setStroke(Color.BLACK);
+	    gc.setLineWidth(2);
+	    Font theFont = Font.font("Times New Roman", FontWeight.BOLD, 48);
+	    gc.setFont(theFont);
+	    gc.fillText("Hello, World!", 60, 50);
+	    gc.strokeText("Hello, World!", 60, 50);
+	    //this.getChildren().add(canvas);
+	    
+	    gc.drawImage(rock, 14, 237);
+	    
 		return startScreen;
 	}
 }
