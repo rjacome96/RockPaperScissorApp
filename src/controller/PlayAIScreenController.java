3d36ea2d3df4c030c1be2a76db72dbc6fbcc1513
Roy O. Jacome <rjacomejr96@aol.com>
2019-06-26 10:44:25 -0400

    Added comments

--- src/controller/PlayAIScreenController.java
+++ src/controller/PlayAIScreenController.java
@@ -29,7 +29,8 @@ public class PlayAIScreenController extends AnchorPane {
 			new Image("File:gameImages/PaperClipArt.png"),
 			new Image("File:gameImages/ScissorClipArt.png")};
 	
-	private static AnimationTimer programButtonAnimation = null;
+	private static final Image AI_DEFAULT_CHOICE = new Image("File:gameImages/AIThinkingClipArt.png");
+	private static AnimationTimer animationTimerSwitch = null;
 		
 	/**
 	 * Overriding the default no arg constructor
@@ -39,10 +40,20 @@ public class PlayAIScreenController extends AnchorPane {
 		
 	}
 
+	/**
+	 * Creates this class Object and calls the method to create the scene
+	 * that this class uses.
+	 * @return The scene that was returned from the {@link createPlayAIScreen}.
+	 */
 	public static Scene getPlayAIScreen() {
 		return new PlayAIScreenController().createPlayAIScreen();
 	}
 
+	/**
+	 * Loads the FXML file into the class and sets this class as the controller.
+	 * As well as sets up the AnimationTimer to move the image of the sliding door.
+	 * @return The scene that this class will uses.
+	 */
 	private Scene createPlayAIScreen() {
 		FXMLLoader loader = new FXMLLoader(getClass().getResource("/view/PlayAIScreenView.fxml"));
 		loader.setController(this);
@@ -55,7 +66,7 @@ public class PlayAIScreenController extends AnchorPane {
 		}
 		
 		
-		programButtonAnimation = new AnimationTimer(){	
+		animationTimerSwitch = new AnimationTimer(){	
 			@Override
 	        public void handle(long currentNanoTime){
 	 
@@ -69,32 +80,57 @@ public class PlayAIScreenController extends AnchorPane {
 		return playAIScreen;
 	}
 	
+	/**
+	 * Method that begins the animation timer.
+	 */
 	public static void startAnimationTimer() {
-		programButtonAnimation.start();
+		animationTimerSwitch.start();
 	}
 	
+	/**
+	 * Method that stops the animation timer.
+	 */
 	public static void stopAnimationTimer() {
-		programButtonAnimation.stop();
+		animationTimerSwitch.stop();
 	}
 	
+	/**
+	 * Method to catch that the user selected the rock.
+	 * @param event
+	 */
 	public void rockSelected(MouseEvent event) {
 		System.out.println("Rock selected");
 	}
 	
+	/**
+	 * Method to catch that the user selected the paper.
+	 * @param event
+	 */
 	public void paperSelected(MouseEvent event) {
 		System.out.println("Paper selected");
 	}
-
+	
+	/**
+	 * Method to catch that the user selected the scissor.
+	 * @param event
+	 */
 	public void scissorSelected(MouseEvent event) {
 		System.out.println("Scissor selected");
 	}
 	
+	/**
+	 * Method that catches when the reset button is clicked
+	 * @param event
+	 */
 	public void restartGame(ActionEvent event) {
-		System.out.println("Restart game button is pressed");
-		resetAIChoice();
-		resetSlidingDoor();
+		restartGame();
 	}
 	
+	/**
+	 * When called, the AI will be given a random choice to display
+	 * the image as its own choice.
+	 * @return The move the that the AI chose.
+	 */
 	public Object makeAIChoose() {
 		// TODO Create the classes and return the appropriate class
 		int random = (int)(Math.random() * 3); 
@@ -102,18 +138,37 @@ public class PlayAIScreenController extends AnchorPane {
         return null;
 	}
 	
+	/**
+	 * Resets the sliding door back to its closing position.
+	 */
 	private void resetSlidingDoor() {
 		slidingDoor.setX(SLIDING_DOOR_X);
 		slidingDoor.setY(SLIDING_DOOR_Y);
 	}
 	
+	/**
+	 * Changes the image for the AI back to default.
+	 */
 	private void resetAIChoice() {
-		aiChoice.setImage(null);
+		aiChoice.setImage(AI_DEFAULT_CHOICE);
 	}
 	
+	/**
+	 * Called when the player clicks on the back button.
+	 * @param e
+	 */
 	public void previousStage(ActionEvent e) {
 		stopAnimationTimer();
 		RockPaperScissorMain.previousStage();
 	}
 
+	/**
+	 * Resets all values and puts the game back to
+	 * the start, playing against the AI.
+	 */
+	private void restartGame() {
+		System.out.println("Restart game button is pressed");
+		resetAIChoice();
+		resetSlidingDoor();
+	}
 }
