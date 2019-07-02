47dd591d3ab7ec1ea844b1c9e88a14f48722ca09
Roy O. Jacome <rjacomejr96@aol.com>
2019-07-02 14:24:43 -0400

    Added more design to the play human screen. Also added the functionaility of the back and reset button.

--- src/controller/PlayHumanScreenController.java
+++ src/controller/PlayHumanScreenController.java
@@ -2,11 +2,22 @@ package controller;
 
 import java.io.IOException;
 
+import application.RockPaperScissorMain;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.text.Text;
 
 public class PlayHumanScreenController extends AnchorPane {
+	
+	@FXML
+	Text resultText;
+	
+	@FXML
+	ImageView player1Image, player2Image;
 
 	private PlayHumanScreenController() {
 		
@@ -30,4 +41,16 @@ public class PlayHumanScreenController extends AnchorPane {
 		return playHumanScreen;
 	}
 	
+	public void resetButtonPressed(ActionEvent event) {
+		System.out.println("Reset button pressed");
+	}
+	
+	/**
+	 * Method is called when the user presses the back button
+	 * to return to the previous screen.
+	 * @param event
+	 */
+	public void backButtonPressed(ActionEvent event) {
+		RockPaperScissorMain.previousStage();
+	}
 }
