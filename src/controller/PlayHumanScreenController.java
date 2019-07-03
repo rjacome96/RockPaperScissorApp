4a926bb2603362b9c48031e6b750182d96a50f78
Roy O. Jacome <rjacomejr96@aol.com>
2019-07-03 09:44:03 -0400

    Added documentation to the methods.

--- src/controller/PlayHumanScreenController.java
+++ src/controller/PlayHumanScreenController.java
@@ -136,66 +136,134 @@ public class PlayHumanScreenController extends AnchorPane {
 		return playHumanScreen;
 	}
 	
+	/**
+	 * Method called when the first player picks {@code Rock}.
+	 * @param event
+	 */
 	public void player1RockSelected(MouseEvent event) {
 		System.out.println("Player 1 Rock Selected");
 		openPlayer1Door();
 	}
 	
+	/**
+	 * Method called when the first player picks {@code Paper}.
+	 * @param event
+	 */
 	public void player1PaperSelected(MouseEvent event) {
 		System.out.println("Player 1 Paper Selected");
 		closePlayer1Door();
 	}
 	
+	/**
+	 * Method called when the first player picks {@code Scissor}.
+	 * @param event
+	 */
 	public void player1ScissorSelected(MouseEvent event) {
 		System.out.println("Player 1 Scissor Selected");
 	}
 	
+	/**
+	 * Method called when the second player picks {@code Rock}.
+	 * @param event
+	 */
 	public void player2RockSelected(MouseEvent event) {
 		System.out.println("Player 2 Rock Selected");
 		openPlayer2Door();
 	}
 	
+	/**
+	 * Method called when the second player picks {@code Paper}.
+	 * @param event
+	 */
 	public void player2PaperSelected(MouseEvent event) {
 		System.out.println("Player 2 Paper Selected");
 		closePlayer2Door();
 	}
 	
+	/**
+	 * Method called when the second player picks {@code Scissor}.
+	 * @param event
+	 */
 	public void player2ScissorSelected(MouseEvent event) {
 		System.out.println("Player 2 Scissor Selected");
 	}
 	
+	/**
+	 * Opens the first player's door.
+	 */
 	private void openPlayer1Door() {
 		openPlayer1SlidingDoor.stop();
 		resetPlayer1SlidingDoor();
 		openPlayer1SlidingDoor.start();
 	}
 	
+	/**
+	 * Closes the first player's door.
+	 */
 	private void closePlayer1Door() {
 		closePlayer1SlidingDoor.stop();
 		closePlayer1SlidingDoor.start();
 	}
 	
+	/**
+	 * Opens the second player's door.
+	 */
 	private void openPlayer2Door() {
 		openPlayer2SlidingDoor.stop();
 		resetPlayer2SlidingDoor();
 		openPlayer2SlidingDoor.start();
 	}
 	
+	/**
+	 * Closes the second player's door.
+	 */
 	private void closePlayer2Door() {
 		closePlayer2SlidingDoor.stop();
 		closePlayer2SlidingDoor.start();
 	}
 	
+	/**
+	 * Resets the first player's door. Back to its 
+	 * closed position.
+	 */
 	private void resetPlayer1SlidingDoor() {
 		player1SlidingDoor.setLayoutX(PLAYER1_CLOSED_SLIDING_DOOR_X);
 	}
 	
+	/**
+	 * Resets the second player's door. Back to its 
+	 * closed position.
+	 */
 	private void resetPlayer2SlidingDoor() {
 		player2SlidingDoor.setLayoutX(PLAYER2_CLOSED_SLIDING_DOOR_X);
 	}
+	
+	/**
+	 * Resets all the players' doors. Back to their original position.
+	 */
 	private void resetDoors() {
 		resetPlayer1SlidingDoor();
 		resetPlayer2SlidingDoor();
+	}
+	
+	/**
+	 * Method that takes the resulting messgae from the match and 
+	 * displays it by making it opaque.
+	 * @param resultMessage The message that resulted in who won or
+	 * if it was a draw.
+	 */
+	private void showResultText(String resultMessage) {
+		resultText.setText(resultMessage);
+		resultText.setOpacity(1);
+	}
+	  
+	/**
+	 * Resets the result text to not being opaque and containing
+	 * empty text.
+	 */
+	private void resetResultText() {
+		resultText.setOpacity(0);
+		resultText.setText("");
 	}
 	/**
 	 * Method called when the reset button is clicked by
@@ -204,6 +272,8 @@ public class PlayHumanScreenController extends AnchorPane {
 	 */
 	public void resetButtonPressed(ActionEvent event) {
 		System.out.println("Reset button pressed");
+		resetDoors();
+		resetResultText();
 	}
 	
 	/**
